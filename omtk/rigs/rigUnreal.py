e23ad33c4e30ea4b75a36b41e1c9b3bf390263de
Unknown <seb.bourgoing@gmail.com>
2020-08-19 21:08:18 -0400

    Base setup with scalar root

--- omtk/rigs/rigUnreal.py
+++ omtk/rigs/rigUnreal.py
@@ -5,98 +5,20 @@ from omtk.core import classRig
 from omtk.core import className
 from omtk.libs import libPymel
 
-log = logging.getLogger('omtk')
+log = logging.getLogger("omtk")
 
-class LumberyardNomenclature(className.BaseName):
-    root_jnt_name = 'root_group'
-    scene_root_name = 'SceneRoot'
-    locomotion_loc_name = 'Locator_Locomotion'
+
+class UnrealNomenclature(className.BaseName):
+    root_jnt_name = "root"
+    scalar_root_name = "scalar_root_jnt"
 
     def __init__(self, *args, **kwargs):
-        super(LumberyardNomenclature, self).__init__(*args, **kwargs)
+        super(UnrealNomenclature, self).__init__(*args, **kwargs)
 
-class LumberyardSceneRoot(classRig.RigGrp):
+
+class RigUnreal(classRig.Rig):
     """
-    Inherit from RigGrp. Represent the scene root node needed to correctly export data for lumberyard.
-    The code have been taken directly from the LumberyardAnimationUtilities.mel that come with lumberyard maya tools
-    """
-    def __init__(self):
-        super(LumberyardSceneRoot, self).__init__()
-
-        self.locomotion_locator = None
-
-    def __createNode__(self, *args, **kwargs):
-        """
-        Create the node the same way it's done in lumberyard tools. At the same time, ensure to create the locomotion
-        locator needed for move by anim feature. It need to be named Locator_Locomotion
-
-        :param args: Additional parameters
-        :param kwargs: Addtional keyword params
-        :return: Return the created scene node
-        """
-
-        # Create the scene root curve
-        scene_root = pymel.curve(d=1, p=[(1, 0, 0), (1.049582, 0, 0.000590718), (1.010888, 0, 0.0685401),
-                                         (1.033656, 0, 0.0685401), (1.067513, 0, 0.0178139),
-                                         (1.10137, 0, 0.0685401), (1.124137, 0, 0.0685401), (1.085562, 0, 0.000590718),
-                                         (1.124255, 0, -0.0673587), (1.101487, 0, -0.0673587),
-                                         (1.067631, 0, -0.0165146), (1.033656, 0, -0.0673587),
-                                         (1.010888, 0, -0.0673587),
-                                         (1.049582, 0, 0.000590718), (1, 0, 0), (0.75, 0, -0.25),
-                                         (0.75, 0, -0.125), (0, 0, -0.125), (-0.125, 0, 0), (-0.125, 0, 0.125),
-                                         (-0.125, 0, 0.75),
-                                         (-0.25, 0, 0.75), (0, 0, 1), (0.000344241, 0, 1.03654),
-                                         (-0.0436663, 0, 0.99124), (-0.0723325, 0, 0.99124), (-0.0723325, 0, 1.104489),
-                                         (-0.0496827, 0, 1.104489), (-0.0496827, 0, 1.015423),
-                                         (0.0370236, 0, 1.104489), (0.0635663, 0, 1.104489), (0.0635663, 0, 0.99879),
-                                         (0.0409165, 0, 0.99879), (0.0409165, 0, 1.0783),
-                                         (0.000344241, 0, 1.03654), (0, 0, 1), (0.25, 0, 0.75), (0.125, 0, 0.75),
-                                         (0.125, 0, 0.125),
-                                         (0.125, 0, 0), (0.125, 0.75, 0), (0.25, 0.75, 0),
-                                         (0, 1, 0), (-0.000470898, 1.056641, 0), (0.0774559, 1.005923, 0),
-                                         (0.0774559, 1.026439, 0),
-                                         (0.012561, 1.065182, 0), (0.0774559, 1.107272, 0),
-                                         (0.0774559, 1.124266, 0), (-0.000470898, 1.073547, 0),
-                                         (-0.0577933, 1.073547, 0),
-                                         (-0.0577933, 1.056641, 0), (-0.000470898, 1.056641, 0),
-                                         (0, 1, 0), (-0.25, 0.75, 0), (-0.125, 0.75, 0), (-0.125, 0, 0),
-                                         (-0.125, 0, 0.75),
-                                         (-0.25, 0, 0.75), (0, 0, 1), (0.25, 0, 0.75), (0.125, 0, 0.75),
-                                         (0.125, 0, 0.125), (0.75, 0, 0.125), (0.75, 0, 0.25), (1, 0, 0)],
-                                 k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-                                    23,
-                                    24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
-                                    45, 46, 47,
-                                    48, 49, 50,
-                                    51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65],
-                                 name=LumberyardNomenclature.scene_root_name)
-
-        #Now create the locomotion locator needed
-        self.locomotion_locator = pymel.createNode('locator').getParent()
-        self.locomotion_locator.rename(LumberyardNomenclature.locomotion_loc_name)
-        self.locomotion_locator.setParent(scene_root)
-        
-        # Rotate it to extract the world different between maya and lumberyard (Same than in 3DS max)
-        pymel.rotate(scene_root, [-90, 180, 0])
-
-        return scene_root
-
-    def unbuild(self, keep_if_children=False, *args, **kwargs):
-        """
-        Unbuild the node
-
-        :param keep_if_children: Will not unbuild the node if it's have children attached on it
-        :param args: Additionnal arguments
-        :param kwargs: Addition keyword arguments
-        """
-        pymel.delete(self.locomotion_locator)
-        self.locomotion_locator = None
-
-        super(LumberyardSceneRoot, self).unbuild()
-
-class RigLumberyard(classRig.Rig):
-    """
-    Custom rig implementation in respect to lumberyard engine.
+    Custom rig implementation in respect to Unreal engine.
     """
 
     DEFAULT_UPP_AXIS = constants.Axis.z
@@ -104,15 +26,15 @@ class RigLumberyard(classRig.Rig):
     LEGACY_LEG_IK_CTRL_ORIENTATION = True
 
     def __init__(self, *args, **kwargs):
-        super(RigLumberyard, self).__init__(*args, **kwargs)
+        super(RigUnreal, self).__init__(*args, **kwargs)
         self._color_ctrl = True
         self.scene_root = None
 
     def _get_nomenclature_cls(self):
         """
-        :return: Return the LumberyardNomenclature class
+        :return: Return the UnrealNomenclature class
         """
-        return LumberyardNomenclature
+        return UnrealNomenclature
 
     def pre_build(self):
         """
@@ -120,21 +42,40 @@ class RigLumberyard(classRig.Rig):
 
         :return:
         """
-        super(RigLumberyard, self).pre_build(create_master_grp=True, create_layer_jnt=True)
+        super(RigUnreal, self).pre_build(create_master_grp=True, create_layer_jnt=True)
 
         #
-        # Create specific group related to squeeze rig convention
+        # Create scalar joint to flip export from maya to unreal
         #
 
         # Build the scene root
-        self.scene_root = self.build_grp(LumberyardSceneRoot, self.scene_root, self.nomenclature.scene_root_name)
+        if pymel.objExists(self.nomenclature.scalar_root_name):
+            self.scene_root = pymel.PyNode(self.nomenclature.scalar_root_name)
+        else:
+            self.scene_root = pymel.joint(name=self.nomenclature.scalar_root_name)
+            self.scene_root.attr("jointOrientX").set(-90)
+            # Setup the scalar root
+
+    def post_build(self):
+        """
+        Call at the end of the rigging process. Nothing is done in the basic rig class, but certain
+        rig definition could need it. Need to be call really at the end to ensure this is the latest
+        things applied on the rig
+        """
+        super(RigUnreal, self).post_build()
 
         # Try to parent the root joint to the scene root
+        if self.scene_root:
+            pymel.parent(self.scene_root, world=True)
+
         if self.grp_jnt:
             self.grp_jnt.setParent(self.scene_root)
         else:
-            log.error('Root joint {} could not be found. Rig export for lumberyard will fail'
-                      .format(self.nomenclature.locomotion_loc_name))
+            log.error(
+                "Root joint {} could not be found. Rig export for Unreal will fail".format(
+                    self.nomenclature.root_jnt_name
+                )
+            )
 
     def post_build_module(self, module):
         """
@@ -142,7 +83,7 @@ class RigLumberyard(classRig.Rig):
 
         :param module: The built module on which we want to do additional changes
         """
-        super(RigLumberyard, self).post_build_module(module)
+        super(RigUnreal, self).post_build_module(module)
 
         # Allow animators to change the rotate order if needed
         for ctrl in module.get_ctrls():
@@ -155,9 +96,10 @@ class RigLumberyard(classRig.Rig):
 
         :return:
         """
-        self.scene_root = self._unbuild_node(self.scene_root, keep_if_children=True)
-        super(RigLumberyard, self)._unbuild_nodes()
-        
-def register_plugin():
-    return RigLumberyard
+        if self.scene_root:
+            pymel.parent(self.scene_root, world=True)
+        super(RigUnreal, self)._unbuild_nodes()
 
+
+def register_plugin():
+    return RigUnreal
